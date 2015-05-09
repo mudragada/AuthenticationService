91d58cd135331d423f7ad312fd7be24dbbc58693
Mudragada, Krishna <v-mudrak-8L@ae.com>
2015-05-09 00:37:40 -0400

    Updated the getAccountById() to use session.get instead of Query. Also added Dao class for data access

---
+++ src/main/java/rfo/ws/rest/AccountRestService.java
@@ -0,0 +1,34 @@
+package rfo.ws.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import rfo.ws.beans.Account;
+import rfo.ws.dao.AccountDao;
+import rfo.ws.dao.AccountDaoImpl;
+
+
+@Path("/account")
+public class AccountRestService {
+    private static AccountDao acDaoObj;
+    public AccountRestService() {
+        acDaoObj = new AccountDaoImpl();
+    }
+
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Account getAccountById(@PathParam("id") int id){
+		Account accountObj = null;
+		try {
+			accountObj = acDaoObj.getAccountById(id);
+		}
+		catch (Exception e) {
+				e.printStackTrace();
+		}  
+		return accountObj;
+	}
+}

b2df43dfa13be317576e557bf887ffadc76542c0
Mudragada, Krishna <v-mudrak-8L@ae.com>
2015-05-13 17:09:47 -0400

    Added UserCredentials bean

--- src/main/java/com/rf/rest/LoginRestService.java
+++ src/main/java/com/rf/rest/LoginRestService.java
@@ -10,6 +10,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.rf.beans.UserCredentials;
+
 @Path("/login")
 public class LoginRestService {
 
@@ -18,14 +20,18 @@ public class LoginRestService {
 	  @Produces(MediaType.APPLICATION_JSON)
 	  @Consumes(MediaType.APPLICATION_JSON)
 	  
-	  public boolean isUserValid(Map<String,Map<String,String>> params) {
-	    for (Entry<String, Map<String, String>> node : params.entrySet()) {
-	    	System.out.println("key=" + node.getKey() + ", value=" + node.getValue());
-	    	for(Entry<String, String> entry : node.getValue().entrySet()){
-	    		System.out.println("key=" + entry.getKey() + ", value=" + entry.getValue());
-	    	}
-		}
-			return true;
-	  }	
-	
+//	  public boolean isUserValid(Map<String,UserCredentials> params) {
+//	    for (Entry<String, Map<String, String>> node : params.entrySet()) {
+//	    	System.out.println("key=" + node.getKey() + ", value=" + node.getValue());
+//	    	for(Entry<String, String> entry : node.getValue().entrySet()){
+//	    		System.out.println("key=" + entry.getKey() + ", value=" + entry.getValue());
+//	    	}
+//		}
+//			return true;
+//	  }	
+	  public boolean isUserValid(Map<String,UserCredentials> params) {
+		  System.out.println(params.get("data").getUsername());
+		  System.out.println(params.get("data").getPassword());
+		  return true;
+	  }
 }
